702d7d723be1d47e1631b1a96807783e22cc8e49
Illia Karpenkov <illiak@flycowgames.com>
2015-01-05 15:48:30 -0800

    Added new console application demonstrating FirefoxDriver usage

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new FirefoxDriver();
+            driver.Url = "http://google.com";
+            Console.ReadKey();
+            driver.Close();
+        }
+    }
+}
